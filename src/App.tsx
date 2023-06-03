37ebe844217eb8a1ed4a86dc383972af2270e457
Yudai Nakajima <kituneudon97@gmail.com>
2023-06-03 18:47:58 +0900

    タイムトラベル機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,15 @@ const Square = ({
   );
 };
 
-const Board = () => {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+const Board = ({
+  xIsNext,
+  squares,
+  onPlay,
+}: {
+  xIsNext: boolean;
+  squares: Array<string>;
+  onPlay: (foo: Array<string>) => void;
+}) => {
   const handleClick = (i: number) => {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -30,9 +35,7 @@ const Board = () => {
     } else {
       nextSquares[i] = "O";
     }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   };
 
   const winner = calculateWinner(squares);
@@ -65,7 +68,51 @@ const Board = () => {
   );
 };
 
-const calculateWinner = (squares: Array<number>) => {
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 1;
+  const currentSquares = history[currentMove];
+
+  const handlePlay = (nextSquares: Array<string>) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  const jumpTo = (nextMove: number) => {
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+const calculateWinner = (squares: Array<string>) => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -87,4 +134,4 @@ const calculateWinner = (squares: Array<number>) => {
   return null;
 };
 
-export default Board;
+export default Game;
